31ed6c23999c0d44593fc6b727c28b5ffa43cfe5
mlasram <mlasram@smart-trade.net>
2017-09-11 16:38:57 +0200

    fix auto completion

--- org.agileware.natural.cucumber.ui/src/org/agileware/natural/cucumber/ui/contentassist/CucumberProposalProvider.java
+++ org.agileware.natural.cucumber.ui/src/org/agileware/natural/cucumber/ui/contentassist/CucumberProposalProvider.java
@@ -11,7 +11,6 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.jface.viewers.StyledString;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.xtext.Assignment;
 import org.eclipse.xtext.RuleCall;
 import org.eclipse.xtext.impl.RuleCallImpl;
 import org.eclipse.xtext.ui.editor.contentassist.ContentAssistContext;
@@ -20,25 +19,30 @@ import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalAcceptor;
 import com.google.inject.Inject;
 
 /**
- * see http://www.eclipse.org/Xtext/documentation/latest/xtext.html#contentAssist on how to customize content assistant
+ * see
+ * http://www.eclipse.org/Xtext/documentation/latest/xtext.html#contentAssist on
+ * how to customize content assistant
  */
 public class CucumberProposalProvider extends AbstractCucumberProposalProvider {
 
 	@Inject
 	private JavaAnnotationMatcher matcher;
-	
+
 	@Inject
 	private AbstractAnnotationDescriptor descriptor;
-	
-	public void complete_Step(EObject model, RuleCall ruleCall, ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
-		if (((RuleCallImpl)context.getLastCompleteNode().getGrammarElement()).getRule().getName().equals("EOL") && context.getPrefix().length() == 0) {
+
+	public void complete_Step(EObject model, RuleCall ruleCall, ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
+		if (((RuleCallImpl) context.getLastCompleteNode().getGrammarElement()).getRule().getName().equals("EOL")
+				&& context.getPrefix().length() == 0) {
 			for (String entry : descriptor.getNames()) {
 				acceptor.accept(createCompletionProposal(entry + " ", context));
 			}
 		}
 	}
-	
-	public void complete_StepDescription(EObject model, RuleCall ruleCall, ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
+
+	public void complete_StepDescription(EObject model, RuleCall ruleCall, ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
 		Collection<String> proposals = matcher.findProposals();
 		for (String proposal : proposals) {
 			String display = proposal;
@@ -51,23 +55,26 @@ public class CucumberProposalProvider extends AbstractCucumberProposalProvider {
 			acceptor.accept(createCompletionProposal(proposal, display, null, context));
 		}
 	}
-	
+
 	@Override
 	public ICompletionProposal createCompletionProposal(String proposal, String displayString, Image image,
 			ContentAssistContext context) {
 		if (isValidProposal(proposal, context.getPrefix(), context)) {
-			return doCreateProposal(proposal, new StyledString(displayString), image, getPriorityHelper()
-					.getDefaultPriority(), context);
+			return doCreateProposal(proposal, new StyledString(displayString), image,
+					getPriorityHelper().getDefaultPriority(), context);
 		}
 		return null;
 	}
-	
+
 	@Override
 	protected boolean isValidProposal(String proposal, String prefix, ContentAssistContext context) {
 		if (proposal == null)
 			return false;
-		if (!proposal.toLowerCase().contains(prefix.toLowerCase()))
-			return false;
+		String[] words = prefix.split(" ");
+		for (String word : words) {
+			if (!proposal.toLowerCase().contains(word.toLowerCase()))
+				return false;
+		}
 		if (getConflictHelper().existsConflict(proposal, context))
 			return false;
 		return true;
